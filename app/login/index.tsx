41cbdd3ba1635bb5e878bca2e1176166504c4d7f
seunghyeon77 <jdyjsh77@naver.com>
2025-10-29 00:42:38 +0900

    feat: login 페이지 변경

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,31 +1,25 @@
-import { Ionicons } from '@expo/vector-icons';
-import { router } from 'expo-router';
-import React from 'react';
-import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { ScrollView, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function LoginScreen() {
+import LoginFooter from "./_components/LoginFooter";
+
+import LoginBody from "./_components/LoginBody";
+import LoginHeader from "./_components/LoginHeader";
+
+export default function Login() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView className="flex-1 bg-white">
-        <View className="flex-row items-center justify-between px-4 py-3">
-          <TouchableOpacity
-            onPress={() => router.back()}
-            className="p-2 -ml-2"
-          >
-            <Ionicons name="arrow-back" size={24} color="#000" />
-          </TouchableOpacity>
-          <Text className="text-lg font-semibold">로그인</Text>
-          <View className="w-8" />
-        </View>
+      <StatusBar barStyle='dark-content' />
 
-        <View className="flex-1 items-center justify-center">
-          <Text className="text-lg font-medium text-gray-900">
-            로그인 페이지입니다
-          </Text>
-        </View>
+      <SafeAreaView className='flex-1 bg-gray-50'>
+        <ScrollView>
+          <LoginHeader />
+          <LoginBody />
+        </ScrollView>
       </SafeAreaView>
+
+      <LoginFooter />
     </>
   );
 }
