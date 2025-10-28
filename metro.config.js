f2882ef8444a458856e0f67e964b9c013b02619c
seunghyeon77 <jdyjsh77@naver.com>
2025-10-28 14:18:29 +0900

    chore: react-native-svg 초기 설정

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,17 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts.push("svg");
+
 module.exports = withNativeWind(config, {
-  input: './global.css',
+  input: "./global.css",
   inlineRem: 16,
 });
---
+++ svg.d.ts
@@ -0,0 +1,7 @@
+// svg.d.ts 또는 global.d.ts 파일
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
