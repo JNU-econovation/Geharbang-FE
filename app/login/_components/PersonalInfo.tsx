4ed9cf06d88d2059d62997dea87f09b9c65d66f6
seunghyeon77 <jdyjsh77@naver.com>
2025-10-29 11:08:34 +0900

    feat: login 개인정보 컴포넌트로 분리

---
+++ app/login/_components/PersonalInfo.tsx
@@ -0,0 +1,31 @@
+import { View } from "react-native";
+
+import Star from "@/public/svgs/Login/star.svg";
+import TextSize from "@/src/components/ui/TextSize";
+
+export default function PersonalInfo() {
+  return (
+    <View className='pt-6'>
+      <View className='flex-row'>
+        <View className='border rounded-sm w-4 h-4' />
+        <View className='pr-1' />
+
+        <Star width={6} height={6} />
+        <View className='pr-1' />
+
+        <View className='border-b border-[#0EA5E9]'>
+          <TextSize size={14} color='#0EA5E9' content='서비스 이용약관' />
+        </View>
+
+        <TextSize size={14} color='#4B5563' content='과' />
+        <View className='pl-1' />
+
+        <View className='border-b border-[#0EA5E9]'>
+          <TextSize size={14} color='#0EA5E9' content='개인정보처리방침' />
+        </View>
+
+        <TextSize size={14} color='#4B5563' content='에 동의합니다.' />
+      </View>
+    </View>
+  );
+}
