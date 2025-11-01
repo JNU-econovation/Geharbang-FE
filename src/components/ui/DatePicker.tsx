afbed13445433e4fe9161c24aacbd6715c19668f
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 01:25:48 +0900

    feat: DatePicker 공통 컴포넌트 추가

---
+++ src/components/ui/DatePicker.tsx
@@ -0,0 +1,42 @@
+import { COLORS } from "@/src/utils/constants/Colors";
+import { Calendar, DateData } from "react-native-calendars";
+
+interface DatePickerProps {
+  onDayPress: (date: DateData) => void;
+  selectedDate: string;
+  size: number;
+  align?: string;
+  minDate?: string;
+  maxDate?: string;
+}
+
+export default function DatePicker({
+  onDayPress,
+  selectedDate,
+  size,
+  align = "center",
+  minDate,
+  maxDate,
+}: DatePickerProps) {
+  return (
+    <Calendar
+      onDayPress={onDayPress}
+      monthFormat="yyyy년 MM월"
+      markedDates={{
+        [selectedDate]: {
+          selected: true,
+          selectedColor: COLORS.PRIMARY.BLUE,
+        },
+      }}
+      style={[
+        { width: size, zIndex: 999, position: "absolute" },
+
+        align === "left" ? { left: 0 } : null,
+        align === "right" ? { right: 0 } : null,
+        align === "center" ? { alignSelf: "center" } : null,
+      ]}
+      minDate={minDate}
+      maxDate={maxDate}
+    />
+  );
+}
