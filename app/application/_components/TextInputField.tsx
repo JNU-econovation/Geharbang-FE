450f9663415046a97e31b71d26181b584766dd0d
eunseo14 <leeeunseo1013@gmail.com>
2025-11-01 23:05:28 +0900

    feat: TextInputField 컴포넌트 추가

---
+++ app/application/_components/TextInputField.tsx
@@ -0,0 +1,66 @@
+import { COLORS } from "@/src/utils/constants/Colors";
+import { Text, TextInput, TextInputProps, View } from "react-native";
+import FieldLabel from "./FieldLabel";
+
+interface TextInputFieldProps {
+  value: string;
+  onChangeText: (text: string) => void;
+  label?: string;
+  isRequired?: boolean;
+  labelSize?: number;
+  placeholder?: string;
+  errorMessage?: string;
+  fontSize?: number;
+  width?: number;
+  height?: number;
+  keyboardType?: TextInputProps["keyboardType"];
+  maxLength?: number;
+  multiline?: boolean;
+  autoCapitalize?: TextInputProps["autoCapitalize"];
+}
+
+export default function TextInputField({
+  value,
+  onChangeText,
+  label,
+  labelSize,
+  isRequired,
+  placeholder,
+  errorMessage,
+  width,
+  height,
+  keyboardType,
+  maxLength,
+  multiline,
+  autoCapitalize,
+}: TextInputFieldProps) {
+  return (
+    <View>
+      {label && (
+        <FieldLabel
+          label={label}
+          isRequired={isRequired}
+          fontSize={labelSize}
+        ></FieldLabel>
+      )}
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        placeholderTextColor={COLORS.GRAY.PLACEHOLDER}
+        className={`border rounded-lg px-3 py-3
+          ${errorMessage ? "border-primary-red" : "border-gray-border"}`}
+        style={{ height: height, width: width }}
+        keyboardType={keyboardType}
+        maxLength={maxLength}
+        multiline={multiline}
+        autoCapitalize={autoCapitalize}
+      />
+      <View className="mt-1 ml-1">
+        <Text className="text-primary-red text-xs">
+          {errorMessage ? errorMessage : " "}
+        </Text>
+      </View>
+    </View>
+  );
+}
