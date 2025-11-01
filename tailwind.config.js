d517647d34a4419c828e1084f8a0fd893a273fb8
eunseo14 <leeeunseo1013@gmail.com>
2025-11-01 23:04:57 +0900

    feat: tailwind 사용자 색상 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
+import { COLORS } from "./src/utils/constants/Colors.ts";
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './App.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
   ],
-  presets: [require('nativewind/preset')],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: {
+          blue: COLORS.PRIMARY.BLUE,
+          red: COLORS.PRIMARY.RED,
+        },
+        gray: {
+          border: COLORS.GRAY.BORDER,
+          text: COLORS.GRAY.TEXT,
+          placeholder: COLORS.GRAY.PLACEHOLDER,
+        },
+      },
       fontFamily: {},
     },
   },
