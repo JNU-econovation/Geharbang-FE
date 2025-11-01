b0680a54ae10d78dd554be0501ec5900973a42a0
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 02:00:37 +0900

    feat: DateInputField 컴포넌트 추가

---
+++ app/application/_components/DateInputField.tsx
@@ -0,0 +1,77 @@
+import DatePicker from "@/src/components/ui/DatePicker";
+import { useDatePicker } from "@/src/hooks/useDatePicker"
+import { COLORS } from "@/src/utils/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, Text, View } from "react-native";
+import FieldLabel from "./FieldLabel";
+
+interface DateInputFieldProps {
+  setSelectedDate: (date: string) => void;
+  selectedDate: string;
+  label: string;
+  isRequired?: boolean;
+  labelSize?: number;
+  size: number;
+  align: "left" | "right" | "center";
+  width?: number;
+  minDate?: string;
+  maxDate?: string;
+}
+
+export default function DateInputField({
+  setSelectedDate,
+  selectedDate,
+  label,
+  isRequired,
+  labelSize,
+  size,
+  align,
+  width,
+  minDate,
+  maxDate,
+}: DateInputFieldProps) {
+  const { toggleCalendar, setToggleCalendar, onDayPress } =
+    useDatePicker(setSelectedDate);
+
+  return (
+    <View>
+      <FieldLabel
+        label={label}
+        isRequired={isRequired}
+        fontSize={labelSize}
+      ></FieldLabel>
+
+      <View style={{ width: width, position: "relative" }}>
+        <Pressable
+          onPress={() => setToggleCalendar((prev) => !prev)}
+          className="flex-row justify-between border rounded-lg px-3 py-3 border-gray-border"
+        >
+          <Text
+            className={`${
+              selectedDate ? "text-black" : "text-gray-placeholder"
+            } text-base`}
+          >
+            {selectedDate ? selectedDate : "yyyy-mm-dd"}
+          </Text>
+          <Ionicons
+            name="calendar-clear-outline"
+            color={COLORS.GRAY.PLACEHOLDER}
+            size={20}
+          ></Ionicons>
+        </Pressable>
+
+        {toggleCalendar && (
+          <DatePicker
+            onDayPress={onDayPress}
+            size={size}
+            align={align}
+            selectedDate={selectedDate}
+            minDate={minDate}
+            maxDate={maxDate}
+          ></DatePicker>
+        )}
+
+      </View>
+    </View>
+  );
+}
