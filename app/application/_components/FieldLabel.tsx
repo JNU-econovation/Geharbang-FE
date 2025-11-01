d455aa6719cb362ead5c6a0cb54bb981e6302ef6
eunseo14 <leeeunseo1013@gmail.com>
2025-11-01 23:06:18 +0900

    feat: FieldLabel 컴포넌트 추가

---
+++ app/application/_components/FieldLabel.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+
+interface FieldLabelProps {
+  label: string;
+  isRequired?: boolean;
+  fontSize?: number;
+}
+
+export default function FieldLabel({
+  label,
+  isRequired,
+  fontSize,
+}: FieldLabelProps) {
+  return (
+    <View className="mb-2 ml-1 flex-row">
+      <Text style={{fontSize}}>{label}</Text>
+      {isRequired ? (
+        <Text > *</Text>
+      ) : (
+        <Text className="text-gray-placeholder"> (선택)</Text>
+      )}
+    </View>
+  );
+}
