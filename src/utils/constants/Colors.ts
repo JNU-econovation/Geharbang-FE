0ca8f1e9138663a354871329c073fb3f52bb6d13
eunseo14 <leeeunseo1013@gmail.com>
2025-11-01 23:02:01 +0900

    feat: 공통 색상 지정

---
+++ src/utils/constants/Colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  GRAY: {
+    BORDER: '#E5E7EB',      
+    TEXT: '#4A5565',     
+    PLACEHOLDER: '#ADAEBC',
+  },
+  PRIMARY:{
+    BLUE: '#1E90FF',
+    RED :'#FB2C36'
+  }
+} as const;
