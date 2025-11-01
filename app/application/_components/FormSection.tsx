a182fef86ba64b3411bfbb87a72105c54f9644f3
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 08:04:36 +0900

    feat: FormSection 컴포넌트 추가

---
+++ app/application/_components/FormSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface FormSectionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function FormSection({ title, children }: FormSectionProps) {
+  return (
+    <View className="bg-white p-4 pb-6 mb-4 rounded-lg shadow-xs"> 
+      <Text className="text-[16px] font-extrabold mb-6">
+        {title}
+      </Text>
+      
+      <View className="gap-2.5"> 
+        {children}
+      </View>
+    </View>
+  );
+}
