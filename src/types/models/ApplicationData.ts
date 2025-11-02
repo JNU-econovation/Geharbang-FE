ff9f5e39776f1449322c359067fcfef1bdf6a441
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 11:13:36 +0900

    feat: ApplicationData 타입 정의

---
+++ src/types/models/ApplicationData.ts
@@ -0,0 +1,13 @@
+export interface ApplicationData {
+  name: string;
+  phoneNumber: string;
+  birthDate: string;
+  gender: string;
+  availableStartDate: string;
+  availableDayOfWeek: string[];
+  selfIntroduction: string;
+  mbti: string;
+  style: string[];
+  instagramId: string;
+  imageUrl: string;
+}
