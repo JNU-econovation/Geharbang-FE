287b73a722d2afd4c9b770c9a92bb0c523e8fcff
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 09:13:29 +0900

    feat: useMulltiSelect 훅 분리

---
+++ src/hooks/useMultiSelect.ts
@@ -0,0 +1,11 @@
+export function useMultiSelect(
+  setSelected: React.Dispatch<React.SetStateAction<string[]>>
+) {
+  const toggleSelect = (value: string) => {
+    setSelected((prev) =>
+      prev.includes(value) ? prev.filter((v) => v !== value) : [...prev, value]
+    );
+  };
+
+  return { toggleSelect };
+}
