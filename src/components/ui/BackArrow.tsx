428cecefb8d611d4b1532d901258921f373f8668
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 20:38:33 +0900

    feat: BackArrow 컴포넌트 기능 수정

--- src/components/ui/BackArrow.tsx
+++ src/components/ui/BackArrow.tsx
@@ -5,11 +5,20 @@ import { TouchableOpacity } from "react-native";
 interface BackArrowProps {
   size: number;
   color: string;
+  onPress?: () => void;
 }
 
-export default function BackArrow({ size, color }: BackArrowProps) {
+export default function BackArrow({ size, color, onPress }: BackArrowProps) {
+  const handlePress = () => {
+    if (onPress) {
+      onPress();
+    } else {
+      router.back();
+    }
+  };
+
   return (
-    <TouchableOpacity onPress={() => router.back()}>
+    <TouchableOpacity onPress={handlePress}>
       <Ionicons name="arrow-back" size={size} color={color} />
     </TouchableOpacity>
   );
