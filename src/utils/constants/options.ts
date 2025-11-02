d41355921432912243e3473dc3e9fb0d0746ceb8
eunseo14 <leeeunseo1013@gmail.com>
2025-11-02 10:51:36 +0900

    feat: 성별 옵션 추가, 옵션 타입 분리

--- src/utils/constants/options.ts
+++ src/utils/constants/options.ts
@@ -1,7 +1,4 @@
-interface Option {
-  value: string;
-  label: string;
-}
+import { Option } from "@/src/types/Option";
 
 export const STYLE_OPTIONS: Option[] = [
   { value: "친근한", label: "#친근한" },
@@ -10,4 +7,15 @@ export const STYLE_OPTIONS: Option[] = [
   { value: "성실한", label: "#성실한" },
   { value: "유머", label: "#유머" },
   { value: "책임감", label: "#책임감" },
+];
+
+export const GENDER_BASIC: Option[] = [
+  { value: "남", label: "남" },
+  { value: "여", label: "여" },
+];
+
+export const GENDER_FULL: Option[] = [
+  { value: "남", label: "남" },
+  { value: "여", label: "여" },
+  { value: "무관", label: "무관" },
 ];
