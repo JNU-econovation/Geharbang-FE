43858b4e50a31f1c1a104d5143b8c15bba332425
eunseo14 <leeeunseo1013@gmail.com>
2025-11-04 14:49:54 +0900

    fix: 대소문자 구분 활성화 하여  파일명 변경

---
+++ src/utils/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  GRAY: {
+    BORDER: '#E5E7EB',      
+    TEXT: '#4A5565',     
+    PLACEHOLDER: '#ADAEBC',
+  },
+  PRIMARY:{
+    BLUE: '#0EA5E9',
+    RED :'#FB2C36'
+  }
+} as const;
