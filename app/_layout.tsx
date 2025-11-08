04696d1366b8f90107fac3fe7c6daa4c1d6c9678
eunseo14 <leeeunseo1013@gmail.com>
2025-11-08 14:06:16 +0900

    refactor: 레이아웃 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -39,20 +40,24 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+const queryClient = new QueryClient();
+
 function RootLayoutNav() {
   return (
     <SafeAreaProvider>
-      <ThemeProvider value={DefaultTheme}>
-        <Stack >
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="application"
-            options={{ headerShown: false, gestureEnabled: false }}
-          />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider value={DefaultTheme}>
+          <Stack screenOptions={{ headerShown: false}}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="login" />
+            <Stack.Screen
+              name="application/create"
+              options={{ gestureEnabled: false }}
+            />
+            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+          </Stack>
+        </ThemeProvider>
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
--- app/application/create/_layout.tsx
+++ app/application/create/_layout.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function ApplicationCreateLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="step2" options={{ headerShown: false, gestureEnabled: false }} />
-      <Stack.Screen name="step3" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false, gestureEnabled: false}}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="step2"/>
+      <Stack.Screen name="result" />
     </Stack>
   );
 }
