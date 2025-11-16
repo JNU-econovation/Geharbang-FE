c114e6c638f07d71ab748860eab2419ef1bf38ac
seunghyeon77 <jdyjsh77@naver.com>
2025-11-16 12:54:42 +0900

    feat: 스텝 근무 정보 컴포넌트 추가

---
+++ app/step/stepDetail/_components/WorkInfo/WorkInfo.tsx
@@ -0,0 +1,46 @@
+import { View } from "react-native";
+
+import SectionYPosition from "../SectionYPosition";
+import WorkInfoDetail from "./WorkInfoDetail";
+
+import Calendar from "@/public/svgs/StepDetail/calendar.svg";
+import Clock from "@/public/svgs/StepDetail/clock.svg";
+import WorkBag from "@/public/svgs/StepDetail/workBag.svg";
+
+interface WorkInfoProps {
+  setSectionYPositions: React.Dispatch<
+    React.SetStateAction<{ [key: string]: number }>
+  >;
+}
+
+export default function WorkInfo({ setSectionYPositions }: WorkInfoProps) {
+  return (
+    <SectionYPosition
+      section='workInfo'
+      content='근무 정보'
+      setSectionYPositions={setSectionYPositions}
+    >
+      <View className='pt-6' />
+
+      <WorkInfoDetail
+        icon={<Calendar width={16} height={16} />}
+        workInfoTitle='근무 시작일'
+        workInfoContent='2024년 3월 1일'
+      />
+
+      <View className='pt-3' />
+      <WorkInfoDetail
+        icon={<WorkBag width={16} height={16} />}
+        workInfoTitle='근무 기간'
+        workInfoContent='3개월 이상'
+      />
+
+      <View className='pt-3' />
+      <WorkInfoDetail
+        icon={<Clock width={16} height={16} />}
+        workInfoTitle='근무 시간'
+        workInfoContent='오전 9시 ~ 오후 6시 (8시간)'
+      />
+    </SectionYPosition>
+  );
+}
