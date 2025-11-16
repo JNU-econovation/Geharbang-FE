09268b29ada558524249ae7a3ca0b155365d58f6
seunghyeon77 <jdyjsh77@naver.com>
2025-11-16 12:50:14 +0900

    feat: 섹션별 스크롤 기능 추가

---
+++ app/step/stepDetail/_components/PressSection/PressSection.tsx
@@ -0,0 +1,53 @@
+import Flex from "@/src/components/layout/Flex/Flex";
+import PressSectionToScroll from "./PressSectionToScroll";
+
+interface PressSectionProps {
+  handleSectionToScroll: (key: string) => void;
+  selectedSection: string;
+}
+
+export default function PressSection({
+  handleSectionToScroll,
+  selectedSection,
+}: PressSectionProps) {
+  return (
+    <Flex items='center' justify='center' dir='row' gap={30}>
+      <PressSectionToScroll
+        section='address'
+        content='주소'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "address"}
+      />
+      <PressSectionToScroll
+        section='workInfo'
+        content='근무 정보'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "workInfo"}
+      />
+      <PressSectionToScroll
+        section='workDate'
+        content='근무일'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "workDate"}
+      />
+      <PressSectionToScroll
+        section='intro'
+        content='소개'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "intro"}
+      />
+      <PressSectionToScroll
+        section='feature'
+        content='특징'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "feature"}
+      />
+      <PressSectionToScroll
+        section='contact'
+        content='연락처'
+        handleSectionToScroll={handleSectionToScroll}
+        isActive={selectedSection === "contact"}
+      />
+    </Flex>
+  );
+}
