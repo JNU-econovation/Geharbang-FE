3a7fe04c251aba9a3593bb76b7e186725a6eac8d
seunghyeon77 <jdyjsh77@naver.com>
2025-11-16 12:57:54 +0900

    feat: 섹션 선택 시 해당 섹션으로 이동하는 커스텀 훅 추가

---
+++ src/hooks/stepDetail/useHandleSection.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+interface useHandleSectionProps {
+  sectionToScroll: (key: string) => void;
+}
+
+export const useHandleSection = ({
+  sectionToScroll,
+}: useHandleSectionProps) => {
+  const [selectedSection, setSelectedSection] = useState("address");
+
+  const handleSectionToScroll = (key: string) => {
+    setSelectedSection(key);
+    sectionToScroll(key);
+  };
+
+  return {
+    selectedSection,
+    handleSectionToScroll,
+  };
+};
