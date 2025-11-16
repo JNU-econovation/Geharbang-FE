83edf3ab23b35453a091b81b3f871b724c06b3ee
seunghyeon77 <jdyjsh77@naver.com>
2025-11-17 00:27:02 +0900

    feat: 연락처 공통 컴포넌트 추가

---
+++ app/step/stepDetail/_components/Contact/Contact.tsx
@@ -0,0 +1,74 @@
+import { View } from "react-native";
+
+import Email from "@/public/svgs/StepDetail/contactIcon/email.svg";
+import Insta from "@/public/svgs/StepDetail/contactIcon/insta.svg";
+import Phone from "@/public/svgs/StepDetail/contactIcon/phone.svg";
+import WebSite from "@/public/svgs/StepDetail/contactIcon/webSite.svg";
+
+import TextSize from "@/src/components/ui/TextSize";
+import ViewContext from "@/src/components/ui/ViewContext/ViewContext";
+import { SetSectionYPositionProps } from "@/src/types/models/stepDetail/SetSectionYPosition";
+
+import SectionYPosition from "../SectionYPosition";
+import ContactCompo from "./ContactCompo";
+
+export default function Contact({
+  setSectionYPositions,
+}: SetSectionYPositionProps) {
+  return (
+    <SectionYPosition
+      section='contact'
+      content='연락처'
+      setSectionYPositions={setSectionYPositions}
+    >
+      <View className='pt-6' />
+
+      <ContactCompo
+        variant='insta'
+        icon={<Insta width={20} height={20} />}
+        title='Instagram'
+        content='@jeju_oceanview_house'
+        iconBg='#F6339A'
+      />
+
+      <View className='pt-4' />
+      <ContactCompo
+        variant='phone'
+        icon={<Phone width={20} height={20} />}
+        title='전화번호'
+        content='010-1234-5678'
+        iconBg='#00C950'
+      />
+
+      <View className='pt-4' />
+      <ContactCompo
+        variant='email'
+        icon={<Email width={20} height={20} />}
+        title='이메일'
+        content='info@jejuoceanview.com'
+        iconBg='#2B7FFF'
+      />
+
+      <View className='pt-4' />
+      <ContactCompo
+        variant='webSite'
+        icon={<WebSite width={20} height={20} />}
+        title='웹사이트'
+        content='jejuoceanview.com'
+        iconBg='#364153'
+      />
+
+      <View className='pt-4' />
+
+      <ViewContext variant='owerMes' height={74}>
+        <View className='px-4 py-3'>
+          <TextSize
+            size={14}
+            color='#973C00'
+            content='💡 궁금한 점이 있으시면 언제든지 연락주세요. 빠르고 친절하게 답변 드리겠습니다!'
+          />
+        </View>
+      </ViewContext>
+    </SectionYPosition>
+  );
+}
---
+++ app/step/stepDetail/_components/Contact/ContactCompo.tsx
@@ -0,0 +1,47 @@
+import { View } from "react-native";
+
+import ContactRedirect from "@/public/svgs/StepDetail/contactIcon/contactRedirect.svg";
+
+import TextSize from "@/src/components/ui/TextSize";
+import ViewContext from "@/src/components/ui/ViewContext/ViewContext";
+
+interface ContactCompoProps {
+  variant: "insta" | "phone" | "email" | "webSite";
+  icon: React.ReactNode;
+  title: string;
+  content: string;
+  iconBg: string;
+}
+
+export default function ContactCompo({
+  variant,
+  icon,
+  title,
+  content,
+  iconBg,
+}: ContactCompoProps) {
+  return (
+    <ViewContext
+      variant={variant}
+      height={74}
+      className='items-center flex-row'
+    >
+      <View className='pr-5' />
+      <View
+        className='h-10 w-10 rounded-full flex items-center justify-center'
+        style={{ backgroundColor: iconBg }}
+      >
+        {icon && icon}
+      </View>
+
+      <View className='pr-3' />
+      <View className='w-60'>
+        <TextSize color='#4A5565' size={12} content={title} />
+        <View className='pt-1' />
+        <TextSize color='#101828' size={14} content={content} />
+      </View>
+
+      <ContactRedirect width={22} height={22} />
+    </ViewContext>
+  );
+}
