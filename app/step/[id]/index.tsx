e9934a8aa8b02d65fa7a3b1a8059c118b4c4f2a1
eunseo14 <leeeunseo1013@gmail.com>
2025-11-19 03:58:51 +0900

    feat: 스텝 관련 파일 생성

---
+++ app/step/[id]/index.tsx
@@ -0,0 +1,4 @@
+import { Text } from "react-native";
+export default function StepDetail() {
+  return <Text className="mt-40">스텝 상세보기 페이지</Text>;
+}
---
+++ app/step/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Step() {
+  return <Text className="mt-40">스텝 목록 페이지</Text>;
+}
