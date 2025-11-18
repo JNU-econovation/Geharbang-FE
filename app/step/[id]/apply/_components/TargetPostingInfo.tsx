f3e2f4263e558bce843cf7fbf11ae6cd6fd7d371
eunseo14 <leeeunseo1013@gmail.com>
2025-11-19 03:35:34 +0900

    feat: TargetPostingInfo 컴포넌트 구현

---
+++ app/step/[id]/apply/_components/TargetPostingInfo.tsx
@@ -0,0 +1,41 @@
+import { Image, View } from "react-native";
+
+import Flex from "@/src/components/layout/Flex";
+import Tag from "@/src/components/ui/Tag";
+import TextSize from "@/src/components/ui/TextSize";
+import { GuestHouseCard } from "@/src/types/models/GuestHouseCard";
+import { COLORS } from "@/src/utils/constants/colors";
+
+interface TargetPostingInfoProps extends GuestHouseCard {
+  region: string;
+}
+
+export default function TargetPostingInfo({
+  imageUrl,
+  name,
+  region,
+  tags,
+}: TargetPostingInfoProps) {
+  return (
+    <View className="bg-[#F9FAFB] px-3 py-4 border border-gray-border">
+      <Flex justify="start" items="center" flexDir="row" gap={10}>
+        <Image src={imageUrl} className="rounded-xl w-16 h-16" />
+        <Flex justify="center" items="start" gap={6}>
+          <TextSize size={17} content={name} />
+          <TextSize size={12} color={COLORS.GRAY.TEXT} content={region} />
+          <Flex
+            justify="start"
+            items="center"
+            flexDir="row"
+            gap={3}
+            flexWrap="wrap"
+          >
+            {tags.map((tag) => (
+              <Tag key={tag} size={12} content={"# " + tag} />
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
+    </View>
+  );
+}
