434032d8ade8dbb97e718957dd25b76eb73812df
seunghyeon77 <jdyjsh77@naver.com>
2025-11-24 21:31:17 +0900

    feat: 동적 라우팅 추가

--- app/step/index.tsx
+++ app/step/index.tsx
@@ -1,14 +1,16 @@
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { Pressable, Text } from "react-native";
 
 import CustomSafeAreaView from "@/src/components/layout/CustomSafeAreaView";
 import BackArrorHeader from "@/src/components/ui/BackArrowHeader";
 
 export default function Step() {
+  const { id } = useLocalSearchParams();
+
   return (
     <CustomSafeAreaView pageColor='bg-white'>
       <BackArrorHeader content='스텝 공고 상세 이동' />
-      <Pressable onPress={() => router.push("/step/stepDetail")}>
+      <Pressable onPress={() => router.push(`/step/stepDetail/${id}`)}>
         <Text>이동하기</Text>
       </Pressable>
     </CustomSafeAreaView>
