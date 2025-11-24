65f2f5f07c4f4a68d05c2a4256f209467611f3dd
eunseo14 <leeeunseo1013@gmail.com>
2025-11-24 18:26:06 +0900

    refactor: CustomSafeAreaView 적용

--- app/step/[id]/apply/result.tsx
+++ app/step/[id]/apply/result.tsx
@@ -1,7 +1,7 @@
 import { router, useLocalSearchParams } from "expo-router";
-import { StatusBar, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
+import CustomSafeAreaView from "@/src/components/layout/CustomSafeAreaView";
 import ResultLayout from "@/src/components/layout/ResultLayout";
 import TextSize from "@/src/components/ui/TextSize";
 
@@ -10,59 +10,56 @@ export default function ApplyResultScreen() {
   const goHome = () => router.replace("/(tabs)");
 
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView className="flex-1 bg-white">
-        <View className="p-3 items-center">
-          <TextSize size={18} content="지원서 작성 완료" />
-        </View>
+    <CustomSafeAreaView pageColor="bg-white">
+      <View className="p-3 items-center">
+        <TextSize size={18} content="지원서 작성 완료" />
+      </View>
 
-        <View className="h-full px-3 pt-8 bg-[#F9FAFB]">
-          {status === "pending" && (
-            <ResultLayout
-              status="pending"
-              title="제출 중..."
-              description="지원서를 제출하고 있습니다"
-              primary={{ label: "제출 중..." }}
-            />
-          )}
+      <View className="h-full px-3 pt-8 bg-[#F9FAFB]">
+        {status === "pending" && (
+          <ResultLayout
+            status="pending"
+            title="제출 중..."
+            description="지원서를 제출하고 있습니다"
+            primary={{ label: "제출 중..." }}
+          />
+        )}
 
-          {status === "success" && (
-            <ResultLayout
-              status="success"
-              title="지원 완료!"
-              description={
-                "사장님이 지원서를 검토 중입니다 \n 보통 1-3일 내 연락드립니다"
-              }
-              primary={{ label: "→ 다른 공고 보기" }}
-              tertiary={{
-                label: "홈으로 돌아가기",
-                onPress: goHome,
-                icon: "home-outline",
-              }}
-            />
-          )}
+        {status === "success" && (
+          <ResultLayout
+            status="success"
+            title="지원 완료!"
+            description={
+              "사장님이 지원서를 검토 중입니다 \n 보통 1-3일 내 연락드립니다"
+            }
+            primary={{ label: "→ 다른 공고 보기" }}
+            tertiary={{
+              label: "홈으로 돌아가기",
+              onPress: goHome,
+              icon: "home-outline",
+            }}
+          />
+        )}
 
-          {status === "error" && (
-            <ResultLayout
-              status="error"
-              title="지원 실패"
-              description={
-                "지원서 제출 중 오류가 발생했습니다.\n잠시 후 다시 시도해주세요."
-              }
-              primary={{
-                label: "다시 시도",
-                onPress: () => router.back,
-              }}
-              tertiary={{
-                label: "홈으로 돌아가기",
-                onPress: goHome,
-                icon: "home-outline",
-              }}
-            />
-          )}
-        </View>
-      </SafeAreaView>
-    </>
+        {status === "error" && (
+          <ResultLayout
+            status="error"
+            title="지원 실패"
+            description={
+              "지원서 제출 중 오류가 발생했습니다.\n잠시 후 다시 시도해주세요."
+            }
+            primary={{
+              label: "다시 시도",
+              onPress: () => router.back(),
+            }}
+            tertiary={{
+              label: "홈으로 돌아가기",
+              onPress: goHome,
+              icon: "home-outline",
+            }}
+          />
+        )}
+      </View>
+    </CustomSafeAreaView>
   );
 }
