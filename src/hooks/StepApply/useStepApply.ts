2d2d5ac6618b85ce02e302ffda2123fa74eefafa
eunseo14 <leeeunseo1013@gmail.com>
2025-11-27 10:26:57 +0900

    feat: 스텝 지원 요청 api 요청 함수, 훅 구현

---
+++ src/hooks/StepApply/useStepApply.ts
@@ -0,0 +1,12 @@
+import { stepApply } from "@/src/services/StepApply/stepApply";
+import { Answer } from "@/src/types/models/Answer";
+import { useMutation } from "@tanstack/react-query";
+
+export function useStepApply(recruitmentId: number) {
+  return useMutation<void, Error, Answer[]>({
+    mutationFn: (answers) => stepApply(recruitmentId, answers),
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+}
---
+++ src/services/StepApply/stepApply.ts
@@ -0,0 +1,12 @@
+import { axiosPrivate } from "@/src/services/api/customAxios";
+import { Answer } from "@/src/types/models/Answer";
+
+export const stepApply = async (
+  recruitmentId: number,
+  answers: Answer[]
+): Promise<void> => {
+  const response = await axiosPrivate.post(
+    `/api/v1/application/staff-recruitment/${recruitmentId}`,
+    { answers }
+  );
+};
