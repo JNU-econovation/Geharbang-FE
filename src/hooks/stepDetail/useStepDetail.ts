368a1d755dc95e4e0220266fbc1aa8c3dbec1547
seunghyeon77 <jdyjsh77@naver.com>
2025-11-27 11:30:32 +0900

    feat: 동적 라우팅 useLocalSearchParams 추가

--- src/hooks/stepDetail/useStepDetail.ts
+++ src/hooks/stepDetail/useStepDetail.ts
@@ -2,11 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getStepDetail } from "@/src/services/StepDetail/stepDetail";
 import { StepDetailResponse } from "@/src/types/models/stepDetail/StepDetailResponse";
+import { useLocalSearchParams } from "expo-router";
 
 export const useStepDetail = () => {
+  const { id } = useLocalSearchParams();
+
   const { data, isPending, ...rest } = useQuery<StepDetailResponse>({
-    queryKey: ["stepDetail"],
-    queryFn: getStepDetail,
+    queryKey: [id, "stepDetail"],
+    queryFn: () => getStepDetail(String(id)),
+    enabled: !!id,
   });
 
   const defaultData: StepDetailResponse = {
--- src/services/StepDetail/stepDetail.ts
+++ src/services/StepDetail/stepDetail.ts
@@ -1,9 +1,11 @@
 import { StepDetailResponse } from "@/src/types/models/stepDetail/StepDetailResponse";
 import { axiosPublic } from "../api/customAxios";
 
-export const getStepDetail = async (): Promise<StepDetailResponse> => {
+export const getStepDetail = async (
+  id: string
+): Promise<StepDetailResponse> => {
   const response = await axiosPublic.get(
-    `/api/v1/staff-recruitment/${1}/details`
+    `/api/v1/staff-recruitment/${id}/details`
   );
   return response.data;
 };
