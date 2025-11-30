cf3703f630fc1233d97ac485d8b4b541e6929a00
eunseo14 <leeeunseo1013@gmail.com>
2025-11-30 18:30:05 +0900

    fix: 대소문자 인식하도록 임시로 폴더명 변경

--- src/services/application/createApplication.ts
+++
@@ -1,9 +0,0 @@
-import { axiosPrivate } from "@/src/services/api/customAxios";
-import { ApplicationData } from "@/src/types/models/application/ApplicationData";
-
-export const createApplication = async (
-  data: ApplicationData
-): Promise<number> => {
-  const response = await axiosPrivate.post("/api/v1/application", data);
-  return response.data.applicationId;
-};
