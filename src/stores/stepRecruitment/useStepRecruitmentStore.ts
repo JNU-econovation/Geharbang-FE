0b2fe5972c30ff518510258ae83c8c43a5eaf55d
조인식 <imsif42@naver.com>
2025-12-10 19:44:44 +0900

    feat: store 생성

--- src/stores/stepRecruitment/useStepRecruitmentStore.ts
+++ src/stores/stepRecruitment/useStepRecruitmentStore.ts
@@ -3,13 +3,17 @@ import { create } from "zustand";
 import { StepRecruitmentStore } from "@/src/types/store/stepRecruitmentStore";
 import { createStep1Slice, initialStep1Data } from "./slice/createStep1Slice";
 import { createStep2Slice, initialStep2Data } from "./slice/createStep2Slice";
-// import { createStep3Slice } from "./slice/createStep3Slice";
+import { createStep3Slice, initialStep3Data } from "./slice/createStep3Slice";
+import { createStep4Slice, initialStep4Data } from "./slice/createStep4Slice";
+import { createStep5Slice, initialStep5Data } from "./slice/createStep5Slice";
 
 export const useStepRecruitmentStore = create<StepRecruitmentStore>()(
   (set, get, api) => ({
     ...createStep1Slice(set, get, api),
     ...createStep2Slice(set, get, api),
-    // ...createStep3Slice(set, get, api),
+    ...createStep3Slice(set, get, api),
+    ...createStep4Slice(set, get, api),
+    ...createStep5Slice(set, get, api),
 
     submitAllData: async () => {
       const state = get();
@@ -17,7 +21,9 @@ export const useStepRecruitmentStore = create<StepRecruitmentStore>()(
       const finalPayload = {
         ...state.step1Data,
         ...state.step2Data,
-        // ...state.step3Data,
+        ...state.step3Data,
+        ...state.step4Data,
+        ...state.step5Data,
       };
 
       console.log("백엔드로 보낼 데이터:", finalPayload);
@@ -27,7 +33,9 @@ export const useStepRecruitmentStore = create<StepRecruitmentStore>()(
       set(() => ({
         step1Data: initialStep1Data,
         step2Data: initialStep2Data,
-        // step3Data: { ...state.step3Data },
+        step3Data: initialStep3Data,
+        step4Data: initialStep4Data,
+        step5Data: initialStep5Data,
       }));
     },
   })
