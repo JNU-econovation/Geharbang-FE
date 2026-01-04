83c42c5469c8fba48595185e5c92ee4b10c19c71
seunghyeon77 <jdyjsh77@naver.com>
2026-01-04 21:18:23 +0900

    refactor: 미리 설정한 색상 상수로 변경

--- app/guestHouse/guestHouseDetail/_components/GuestHouseInfo.tsx
+++ app/guestHouse/guestHouseDetail/_components/GuestHouseInfo.tsx
@@ -6,6 +6,7 @@ import Coffee from "@/public/svgs/GuestHouse/coffee.svg";
 import Wind from "@/public/svgs/GuestHouse/wind.svg";
 import TextSize from "@/src/components/ui/TextSize";
 import { SetSectionYPositionProps } from "@/src/types/models/stepDetail/SetSectionYPosition";
+import { COLORS } from "@/src/utils/constants/colors";
 
 export default function GuestHouseInfo({
   setSectionYPositions,
@@ -17,7 +18,9 @@ export default function GuestHouseInfo({
       setSectionYPositions={setSectionYPositions}
     >
       <View className='pt-6' />
-      <View className='p-4 border border-[#E5E7EB] rounded-lg gap-2'>
+      <View
+        className={`p-4 border border-[${COLORS.GRAY.BORDER}] rounded-lg gap-2`}
+      >
         <View className='flex-row items-center gap-3'>
           <Coffee width={16} height={16} />
           <TextSize size={17} color='#101828' content='제공 편의시설' />
@@ -34,8 +37,25 @@ export default function GuestHouseInfo({
           <TextSize size={17} color='#101828' content='게스트하우스 분위기' />
         </View>
 
-        <View className='self-start px-3 py-2 rounded-2xl bg-[#0EA5E9]/10'>
-          <TextSize color='#0EA5E9' size={14} content='#조용한' />
+        <View className='flex-row gap-2'>
+          <View
+            className={`self-start px-3 py-2 rounded-2xl bg-[${COLORS.PRIMARY.BLUE}]/10`}
+          >
+            <TextSize
+              color={`${COLORS.PRIMARY.BLUE}`}
+              size={14}
+              content='#조용한'
+            />
+          </View>
+          <View
+            className={`self-start px-3 py-2 rounded-2xl bg-[${COLORS.PRIMARY.BLUE}]/10`}
+          >
+            <TextSize
+              color={`${COLORS.PRIMARY.BLUE}`}
+              size={14}
+              content='#조용한'
+            />
+          </View>
         </View>
       </View>
     </SectionYPosition>
