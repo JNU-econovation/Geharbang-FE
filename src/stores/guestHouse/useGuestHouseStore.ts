2d0a194723c3faf0bd9b9e424660342b41b7babb
조인식 <imsif42@naver.com>
2026-01-04 20:23:42 +0900

    feat: 게스트하우스 등록 스토어 & 슬라이스 추가

---
+++ src/stores/guestHouse/useGuestHouseStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { GuestHouseStore } from "@/src/types/store/guestHouseStore";
+import {
+  createStep1Slice,
+  initialStep1Data,
+} from "./slice/createStep1Slice";
+import {
+  createStep2Slice,
+  initialStep2Data,
+} from "./slice/createStep2Slice";
+import {
+  createStep3Slice,
+  initialStep3Data,
+} from "./slice/createStep3Slice";
+import {
+  createStep4Slice,
+  initialStep4Data,
+} from "./slice/createStep4Slice";
+
+export const useGuestHouseStore = create<GuestHouseStore>()((set, get, api) => ({
+  ...createStep1Slice(set, get, api),
+  ...createStep2Slice(set, get, api),
+  ...createStep3Slice(set, get, api),
+  ...createStep4Slice(set, get, api),
+
+  resetAllData: () => {
+    set(() => ({
+      step1Data: initialStep1Data,
+      step2Data: initialStep2Data,
+      step3Data: initialStep3Data,
+      step4Data: initialStep4Data,
+    }));
+  },
+}));
