6bfcc67120bfa76769a420f3f224b0e7b064daa3
조인식 <imsif42@naver.com>
2026-01-07 14:48:27 +0900

    refactor: 타입 재수정

--- app/guestHouse/enroll/addRoomForm.tsx
+++ app/guestHouse/enroll/addRoomForm.tsx
@@ -7,7 +7,6 @@ import { useGuestHouseStore } from '@/src/stores/guestHouse/useGuestHouseStore';
 import { File } from '@/src/types/File';
 import { Feather } from '@expo/vector-icons';
 import { router, useLocalSearchParams } from 'expo-router';
-import { nanoid } from 'nanoid';
 import React, { useEffect, useState } from 'react';
 import {
   ScrollView,
@@ -132,7 +131,7 @@ export default function AddRoomForm() {
 
     const roomPayload = {
       ...roomData,
-      id: isEditMode ? editId : nanoid(),
+      id: isEditMode ? editId : Date.now().toString(),
     };
 
     if (isEditMode && editId) {
