58a7e0a37569e591ee09090e7d217fef36021800
eunseo14 <leeeunseo1013@gmail.com>
2026-01-22 22:01:09 +0900

    refactor: 홈화면 밑 여백 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,19 @@
-import CustomSafeAreaView from '@/src/components/layout/CustomSafeAreaView';
-import React from 'react';
-import { ScrollView, View } from 'react-native';
+import CustomSafeAreaView from "@/src/components/layout/CustomSafeAreaView";
+import React from "react";
+import { ScrollView, View } from "react-native";
 
-import AdBanner from '../home/_components/AdBanner';
-import { GuesthouseSection } from '../home/_components/GuesthouseSection';
-import HomeHeader from '../home/_components/HomeHeader';
-import { StepRecruitmentSection } from '../home/_components/StepRecruitmentSection';
+import AdBanner from "../home/_components/AdBanner";
+import { GuesthouseSection } from "../home/_components/GuesthouseSection";
+import HomeHeader from "../home/_components/HomeHeader";
+import { StepRecruitmentSection } from "../home/_components/StepRecruitmentSection";
 
 export default function HomeScreen() {
   return (
-    <CustomSafeAreaView pageColor="bg-white" topOnly={true}>
+    <CustomSafeAreaView pageColor='bg-white' topOnly={true}>
       <HomeHeader />
 
-      <ScrollView className="py-4 flex-1 ">
-        <View className="w-full items-center gap-9">
+      <ScrollView className='py-4 flex-1 '>
+        <View className='w-full items-center gap-9 mb-8'>
           <AdBanner />
           <GuesthouseSection />
           <StepRecruitmentSection />
