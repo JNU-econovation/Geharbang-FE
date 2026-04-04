96b8535c1ab7976b242e0ba58d01c9004c236cb5
Superkid0714 <kid1412wer@gmail.com>
2026-04-05 06:11:47 +0900

    fix: 403 에러 발생 시 인증 필요 알림 추가

--- src/hooks/stepRecruitment/useCreateStaffRecruitment.ts
+++ src/hooks/stepRecruitment/useCreateStaffRecruitment.ts
@@ -1,6 +1,8 @@
 import { createStaffRecruitment } from '@/src/services/step/createStaffRecruitment';
 import { StaffRecruitmentRequest } from '@/src/types/models/stepRecruitment/StaffRecruitmentRequest';
 import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { Alert } from 'react-native';
 
 export const useCreateStaffRecruitment = () => {
   return useMutation<number, Error, StaffRecruitmentRequest>({
@@ -9,6 +11,10 @@ export const useCreateStaffRecruitment = () => {
       console.log('공고 등록 성공, ID:', staffRecruitmentId);
     },
     onError: (err) => {
+      if (axios.isAxiosError(err) && err.response?.status === 403) {
+        Alert.alert('인증 필요', '스텝 공고 등록은 인증서 심사가 완료된 사장님만 가능합니다.');
+        return;
+      }
       console.error('공고 등록 실패:', err);
     },
   });
