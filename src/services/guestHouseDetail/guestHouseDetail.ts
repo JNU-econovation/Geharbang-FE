2e83040c1ef266de25ccda4d337b4f3968daaf25
Superkid0714 <kid1412wer@gmail.com>
2026-05-11 07:00:18 +0900

    fix: axiosPublic를 axiosOptionalAuth로 변경하여 게스트 하우스 상세 정보 요청 처리

--- src/services/guestHouseDetail/guestHouseDetail.ts
+++ src/services/guestHouseDetail/guestHouseDetail.ts
@@ -1,9 +1,11 @@
 import { GuestHouseDetailResponse } from "@/src/types/guestHouseDetail/GuestHouseDetailResponse";
-import { axiosPublic } from "../api/customAxios";
+import { axiosOptionalAuth } from "../api/customAxios";
 
 export const getGuestHouseDetail = async (
   id: string
 ): Promise<GuestHouseDetailResponse> => {
-  const response = await axiosPublic.get(`/api/v1/guest-houses/${id}/details`);
+  const response = await axiosOptionalAuth.get(
+    `/api/v1/guest-houses/${id}/details`
+  );
   return response.data;
 };
