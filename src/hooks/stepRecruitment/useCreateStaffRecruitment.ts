0037724bd035afa1ed3b870627d25afecc9bfb61
Superkid0714 <kid1412wer@gmail.com>
2026-06-02 02:11:33 +0900

    refactor: 스텝 공고 등록 성공 로그 제거

--- src/hooks/stepRecruitment/useCreateStaffRecruitment.ts
+++ src/hooks/stepRecruitment/useCreateStaffRecruitment.ts
@@ -7,9 +7,6 @@ import { Alert } from 'react-native';
 export const useCreateStaffRecruitment = () => {
   return useMutation<number, Error, StaffRecruitmentRequest>({
     mutationFn: createStaffRecruitment,
-    onSuccess: (staffRecruitmentId) => {
-      console.log('공고 등록 성공, ID:', staffRecruitmentId);
-    },
     onError: (err) => {
       if (axios.isAxiosError(err) && err.response?.status === 403) {
         Alert.alert('인증 필요', '스텝 공고 등록은 인증서 심사가 완료된 사장님만 가능합니다.');
