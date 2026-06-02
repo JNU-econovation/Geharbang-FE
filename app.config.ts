66d7b880ee0eeb97544db186c102586d9f12b0c6
Superkid0714 <kid1412wer@gmail.com>
2026-06-02 17:00:14 +0900

    fix: 앱 버전 및 버전 코드 업데이트

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ if (sentryOrg && sentryProject && sentryAuthToken) {
 const config: ExpoConfig = {
   name: "게하르방",
   slug: "Geharbang-FE",
-  version: "1.2.0",
+  version: "1.2.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   scheme: "geharbang",
@@ -58,7 +58,7 @@ const config: ExpoConfig = {
   },
 
   android: {
-    versionCode: 8,
+    versionCode: 9,
     softwareKeyboardLayoutMode: "resize",
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
